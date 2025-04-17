d33c06d3cb513038ffc2cb2a189d57d792193f00
Valentin FOURNY <fourny.valentin@gmail.com>
2025-04-17 15:38:13 +0200

    test(subject): Add ocmmit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-export default {
+import type { UserConfig } from '@commitlint/types'
+
+export const defaultConfig: UserConfig = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'type-enum': [
@@ -18,5 +20,44 @@ export default {
         'chore',
       ],
     ],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
   },
 }
+
+export const createCommitlintMonorepoConfig = (options: {
+  scopes?: string[]
+  extraRules?: UserConfig['rules']
+}): UserConfig => {
+  const { scopes = [], extraRules = {} } = options
+
+  return {
+    extends: ['@commitlint/config-angular'],
+    parserPreset: {
+      parserOpts: {
+        headerPattern:
+          /^(\w*)(?:\(([\w$.\-*/ ]+)(?::([\w$.\-*/ ]+))?\))?: (.*)$/,
+        headerCorrespondence: ['type', 'scope', 'subScope', 'subject'],
+      },
+    },
+    rules: {
+      ...defaultConfig.rules,
+      'scope-enum': [2, 'always', scopes],
+      'custom-subscope-validation': [2, 'always'],
+      ...extraRules,
+    },
+    plugins: [
+      {
+        rules: {
+          'custom-subscope-validation': ({ subScope }: any) => {
+            if (!subScope) return [false, '']
+            return [true, '']
+          },
+        },
+      },
+    ],
+  }
+}
+
+export default defaultConfig
