06da3db2bf460f8e3fc407cb799ff8548ff62fab
Valentin FOURNY <fourny.valentin@gmail.com>
2025-12-23 22:39:47 +0100

    fix(eslint): add exlcusion

--- configs/eslint/base.ts
+++ configs/eslint/base.ts
@@ -119,7 +119,7 @@ export const commonIgnores: Linter.Config = {
  * Allow default exports and relative imports in config files
  */
 export const configFilesOverride: Linter.Config = {
-  files: ['*.config.{js,ts}'],
+  files: ['**/*.config.{js,ts}'],
   rules: {
     'import/no-default-export': 'off',
     'no-restricted-imports': 'off',
